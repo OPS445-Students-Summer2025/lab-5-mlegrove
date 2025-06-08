6060036cae723d265942f8c584bdf25313965dc4
Mark Legrove <mlegrove@myseneca.ca>
2025-06-08 10:42:51 -0400

    Lab 5 complete

--- lab5a.py
+++ lab5a.py
@@ -2,13 +2,10 @@
 # Author ID: mlegrove
 
 def read_file_string(file_name):
-    """Takes file_name as string for a file name, returns its entire contents as a string"""
     with open(file_name, 'r') as f:
         return f.read()
 
 def read_file_list(file_name):
-    """Takes a file_name as string for a file name, 
-    return its entire contents as a list of lines without new-line characters"""
     with open(file_name, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
